e055c13d4f38fe39572835575c38f0b8e9ed10a4
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-06-27 17:08:02 -0300

    Usando Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
 
-  const name = "Márcio"
+  const nome = "Maria"
 
-  const newName = name.toUpperCase()
-
-  function sum(a,b){
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
+  const name = "Márcio"  
   return (
-    <div className="App">
-      <h1>Olá, {newName}</h1>
-      <p>Meu primeiro App</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha imagem" />
+    <div className="App">       
+      <SayMyName nome="Márcio" />
+      <SayMyName nome="Pedro" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Márcio" idade="43" profissao="Programador" foto="https://via.placeholder.com/150" />
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import Frase from "./frase"
+
+function HellowWorld(){
+    return (
+       <div>
+        <Frase />
+         <h1>Meu primeiro componente</h1>
+         <Frase />
+       </div> 
+    )
+}
+
+export default HellowWorld
