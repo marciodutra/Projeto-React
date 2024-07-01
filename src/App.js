bf1c281d04ec10d8145b1d6abedf48fcc2582beb
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-07-01 12:48:29 -0300

    Trabalhando com eventos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/frase'
-import List from './components/List'
+import Evento from './components/Evento'
+import Form from './components/Form';
 
 function App() {
-
-  const nome = "Maria"
-
-  const name = "Márcio"  
+    
   return (
     <div className="App">
-      <h1>Testando css</h1>
-      <Frase />
-      <Frase />       
-      <SayMyName nome="Márcio" />
-      <SayMyName nome="Pedro" />
-      <SayMyName nome={nome} />
-      <Pessoa nome="Márcio" idade="43" profissao="Programador" foto="https://via.placeholder.com/150" />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
+      
     </div>
-  );
+  )
 }
 
 export default App;
